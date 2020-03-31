76295c9a141cbf89214b39994b6755bed8d4850b
martypowell <jpowel25@gmail.com>
2020-03-31 19:27:24 -0400

    added config, and implemented shcedule logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
+import "whatwg-fetch";
+
+import React, { useState } from "react";
+
 import Autocomplete from "./components/Autocomplete";
-import React from "react";
+import { Config } from "@baltimorecounty/javascript-utilities";
+import Schedule from "./components/Schedule";
+
+const { setConfig, getValue } = Config;
+
+const testApiRoot =
+  "https://testservices.baltimorecountymd.gov/api/hub/collectionSchedule";
+const prodApiRoot =
+  "https://services.baltimorecountymd.gov/api/hub/collectionSchedule";
+
+// HACK - the Config utility does not account for beta.
+// TODO: This will need to be addressed when we get closer to launch
+const localApiRoot =
+  window.location.hostname.indexOf("beta") > -1
+    ? testApiRoot
+    : "http://localhost:53001/api/Schedule";
+
+const configValues = {
+  local: {
+    apiRoot: localApiRoot
+  },
+  development: {
+    apiRoot: testApiRoot
+  },
+  staging: {
+    apiRoot: testApiRoot
+  },
+  production: {
+    apiRoot: prodApiRoot
+  }
+};
+
+setConfig(configValues);
+
+const fetchAddresses = addressQuery =>
+  fetch(`${getValue("apiRoot")}/${addressQuery}`).then(res => res.json());
 
 function App() {
-  const suggest = (query, populateResults) => {
-    const results = ["France", "Germany", "United Kingdom"];
-    const filteredResults = results.filter(
-      result => result.indexOf(query) !== -1
-    );
-    populateResults(["400 Washington Ave"]);
+  const [schedule, setSchedule] = useState({});
+  const [results, setResults] = useState([]);
+  const suggest = async (query, populateResults) => {
+    const addresses = await fetchAddresses(query);
+    setResults(addresses);
+    const filteredResults = addresses.map(result => result.address);
+    populateResults(filteredResults);
+  };
+
+  const handleValueSelect = selectedValue => {
+    if (selectedValue) {
+      setSchedule(results.find(result => result.address === selectedValue));
+    }
   };
 
   return (
@@ -16,7 +62,15 @@ function App() {
         id="address-lookup"
         label="Find Your Collection Schedule"
         suggest={suggest}
+        onConfirm={handleValueSelect}
+        minLength={3}
       />
+      {Object.keys(schedule).length > 0 && (
+        <div className="results">
+          <p>Selected Address: {schedule.address}</p>
+          <Schedule schedule={schedule} />
+        </div>
+      )}
     </div>
   );
 }
