f237fdba88d12e09180c65e9514a4bd674bbcf5b
martypowell <jpowel25@gmail.com>
2020-03-31 10:43:53 -0400

    added basic label an dinptu to the app for address earch

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 test("renders learn react link", () => {
-  const { getByLabel } = render(<App />);
-  const inputElement = getByLabel(/find your collection schedule/i);
+  const { getByLabelText } = render(<App />);
+  const inputElement = getByLabelText(/find your collection schedule/i);
   expect(inputElement).toBeInTheDocument();
 });
