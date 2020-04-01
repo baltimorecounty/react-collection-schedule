ea49e51292d70c93098ca22dcff1d333250e59b8
martypowell <jpowel25@gmail.com>
2020-04-01 15:54:56 -0400

    added alerts for not schedule dwelling types

---
+++ src/components/CommercialAlert.jsx
@@ -0,0 +1,21 @@
+import { Alert } from "@baltimorecounty/dotgov-components";
+import React from "react";
+
+export default (
+  <Alert
+    className="status"
+    type="warning"
+    icon="far fa-exclamation-triangle"
+    status="Invalid Address"
+  >
+    <p>
+      The address you entered is a commercial property or apartment building,
+      and therefore is not included in the County’s standard collection
+      schedule.
+    </p>
+    <p>
+      Please contact you property manager for collection details at this
+      location.
+    </p>
+  </Alert>
+);
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Table,
   TableBody,
   TableCell,
@@ -7,14 +8,25 @@ import {
   TableRow
 } from "@baltimorecounty/dotgov-components";
 
+import CommercialAlert from "./CommercialAlert";
+import InActiveRouteAlert from "./InActiveRouteAlert";
 import PropTypes from "prop-types";
 import React from "react";
 
 const Schedule = ({ schedule = {} }) => {
-  const { collectionSchedules } = schedule;
+  const { collectionSchedules, isSingleFamilyHome, isActiveRoute } = schedule;
   const hasAtLeastOneSchedule = collectionSchedules.some(
     schedule => schedule.nextCollectionDate
   );
+
+  if (!isActiveRoute) {
+    return <InActiveRouteAlert />;
+  }
+
+  if (!isSingleFamilyHome) {
+    return <CommercialAlert />;
+  }
+
   return hasAtLeastOneSchedule ? (
     <Table>
       <TableHead>
