c42faf74ae752351e4928eff2d859684c2865088
martypowell <jpowel25@gmail.com>
2020-04-30 11:39:04 -0400

    added pdf check

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,22 +1,15 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeadCell,
-  TableRow,
-} from "@baltimorecounty/dotgov-components";
-
 import CommercialAlert from "./CommercialAlert";
 import InActiveRouteAlert from "./InActiveRouteAlert";
 import PropTypes from "prop-types";
 import React from "react";
+import ScheduleTable from "./ScheduleTable";
 
 const Schedule = ({ schedule = {} }) => {
   const {
     collectionSchedules = [],
     isSingleFamilyHome,
     isActiveRoute,
+    pdfLink,
   } = schedule;
 
   if (!isActiveRoute) {
@@ -31,59 +24,20 @@ const Schedule = ({ schedule = {} }) => {
     (schedule) => schedule.nextCollectionDate
   );
 
-  const getIconClass = (name) => {
-    switch (name.toLowerCase()) {
-      case "trash":
-        return "far fa-trash-alt";
-      case "recycling":
-        return "far fa-recycle";
-      case "yard waste":
-        return "far fa-leaf";
-      default:
-        return "";
-    }
-  };
-
-  return hasAtLeastOneSchedule ? (
-    <Table className="table-fixed">
-      <TableHead>
-        <TableRow>
-          <TableHeadCell>Type</TableHeadCell>
-          <TableHeadCell>Collection Occurs</TableHeadCell>
-          <TableHeadCell>Next Collection</TableHeadCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {collectionSchedules.map(
-          ({ name, collectionDays, nextCollectionDate }, index) => (
-            <TableRow key={name}>
-              <TableCell style={{ verticalAlign: "middle" }}>
-                <i
-                  className={`d-sm-none d-none d-md-inline ${getIconClass(
-                    name
-                  )} fa-2x`}
-                  style={{ marginRight: "10px" }}
-                  aria-hidden="true"
-                ></i>
-                {name}
-              </TableCell>
-              <TableCell>
-                {collectionDays.length > 0
-                  ? collectionDays.join(",")
-                  : "No collection days specified for this type."}
-              </TableCell>
-              <TableCell>
-                {nextCollectionDate
-                  ? new Date(nextCollectionDate).toLocaleDateString()
-                  : "n / a"}
-              </TableCell>
-            </TableRow>
-          )
-        )}
-      </TableBody>
-    </Table>
-  ) : (
-    <p>No schedules have been found for this property.</p>
+  return (
+    <div>
+      {hasAtLeastOneSchedule ? (
+        <ScheduleTable collectionSchedules={collectionSchedules} />
+      ) : (
+        <p>No schedules have been found for this property.</p>
+      )}
+      {pdfLink && (
+        <>
+          <h3>Download</h3>
+          <a href={pdfLink}>Download your complete four-year schedule.</a>
+        </>
+      )}
+    </div>
   );
 };
 
