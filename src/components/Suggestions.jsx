4cd10823b69985d02c7f9afb1efc9c538182b747
martypowell <jpowel25@gmail.com>
2020-05-04 16:22:28 -0400

    cleaned up form to match routing

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Suggestions = ({ suggestions = [] }) => (
   <div>
     <h3>Did you mean?</h3>
-    {suggestions.map((text, magicKey) => (
-      <a key={magicKey} href={`?suggestion=${text}`}>
-        {text}
-      </a>
-    ))}
+    <ul>
+      {suggestions.map((text, magicKey) => (
+        <li key={magicKey}>
+          <Link to={`/${text}`}>{text}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
