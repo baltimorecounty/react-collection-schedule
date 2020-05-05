ff5d9e2372b352ac06471f5ca96abec40ba5ecff
martypowell <jpowel25@gmail.com>
2020-05-05 14:41:35 -0400

    cleaned up function and added summary

--- src/common/Suggestions.js
+++ src/common/Suggestions.js
@@ -2,12 +2,14 @@ import { Config } from "@baltimorecounty/javascript-utilities";
 import Fetch from "./Fetch";
 const { getValue } = Config;
 
-const GetSuggestions = async (query) => {
-  const { suggestions = [] } = await Fetch("address", {
+/**
+ *  Get suggestions for a given address query
+ * @param {string} query partial address
+ */
+const GetSuggestions = async (query) =>
+  await Fetch("address", {
     endpoint: getValue("suggest"),
     queryString: `?partialAddress=${query}`,
   });
-  return suggestions.map(({ text }) => text);
-};
 
 export { GetSuggestions };
