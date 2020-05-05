1a97541ef83d3f1f272fd308061d935536015793
martypowell <jpowel25@gmail.com>
2020-05-05 14:41:52 -0400

    modified compoent to handle suggestions with key

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Suggestions = ({ suggestions = [] }) => (
-  <div>
-    <h3>Did you mean?</h3>
-    <ul>
-      {suggestions.map((text, magicKey) => (
-        <li key={magicKey}>
-          <Link to={`/${text}`}>{text}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const Suggestions = ({ suggestions = [] }) =>
+  console.log(suggestions) || (
+    <div>
+      <h3>Did you mean?</h3>
+      <ul>
+        {suggestions.map(({ text, magicKey }) => (
+          <li key={magicKey}>
+            <Link to={`/${text}`}>{text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
 export default Suggestions;
