f2c9af7fee11fc06560c3eabd9040e3caa3c5a93
martypowell <jpowel25@gmail.com>
2020-05-14 10:21:22 -0400

    removed status as it wasn't right

--- src/components/SomethingWentWrongAlert.jsx
+++ src/components/SomethingWentWrongAlert.jsx
@@ -2,12 +2,7 @@ import { Alert } from "@baltimorecounty/dotgov-components";
 import React from "react";
 
 const SomethingWentWrongAlert = () => (
-  <Alert
-    className="status"
-    type="warning"
-    icon="far fa-exclamation-triangle"
-    status="Invalid Address"
-  >
+  <Alert className="status" type="warning" icon="far fa-exclamation-triangle">
     <p>Something went wrong. Please try again in a couple of minutes.</p>
   </Alert>
 );
